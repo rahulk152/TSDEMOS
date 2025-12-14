060418473dd04f8094cddffd90e60e500f307886
rahulk152 <rahulkumaraiet@yahoo.in>
2025-12-14 13:40:02 +0530

    Day3 & Day 4

---
+++ Day4/TernaryOperator.ts
@@ -0,0 +1,10 @@
+//Ternary/Conditional Operator
+// Syntax: condition ? expr1 : expr2
+
+let a2:number=10 , b1:number=20;
+let max:number = (a2>b1) ? a2 : b1;
+console.log("Maximum Value is:",max);
+
+let personAge:number=17;
+let eligibility:string = (personAge>=18) ? "Eligible to Vote" : "Not Eligible to Vote";
+console.log("Voting Eligibility:",eligibility);
